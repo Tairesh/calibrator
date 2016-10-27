a597fd35251862c8df3d5471a6c0d6a7a838c835
Ilya Agafonov <tairesh.rus@gmail.com>
2016-10-27 12:15:15 +0500

    подстройка окна приложения под высоту

---
+++ web/js/script.js
@@ -0,0 +1,18 @@
+
+if (typeof VK !== 'undefined') {
+    VK.init({apiId: 4540646});
+}
+
+$(function () {
+    if (typeof VK !== 'undefined') {
+        setInterval(function () {
+            var newheight = $(body).height();
+            if (newheight < 600) newheight = 600;
+
+            VK.callMethod("resizeWindow", 1000, newheight);
+        }, 500);
+    }
+//    if (self.parent.frames.length === 0) { // мы не в приложении вк
+//        $('#account_settings_button').show();
+//    }
+})

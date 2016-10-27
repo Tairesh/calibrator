cd5517ec3eb444bf528d7b81e38a0b9c0daf2357
Ilya Agafonov <tairesh.rus@gmail.com>
2016-10-27 12:22:41 +0500

    fix

--- web/js/script.js
+++ web/js/script.js
@@ -3,12 +3,17 @@ if (typeof VK !== 'undefined') {
     VK.init({apiId: 4540646});
 }
 
-$(function () {
+function resizeVkAppWindow() {
     if (typeof VK !== 'undefined') {
-        setInterval(function () {
-            var newheight = $('#main-container').height();
-            VK.callMethod("resizeWindow", 1000, newheight < 600 ? 600 : newheight);
-        }, 500);
+        var newheight = $('#main-container').height();
+        VK.callMethod("resizeWindow", 1000, newheight < 600 ? 600 : newheight);
+    }
+}
+
+$(function () {    
+    if (typeof VK !== 'undefined') {
+        setInterval(resizeVkAppWindow, 500);
+        $.ajaxStop(resizeVkAppWindow);
     }
 //    if (self.parent.frames.length === 0) { // мы не в приложении вк
 //        $('#account_settings_button').show();

41cc39d105384d5211cd8618eba4fcf4a8f1dc7c
Ilya Agafonov <tairesh.rus@gmail.com>
2016-12-15 17:02:11 +0500

    Рефакторинг JS (close issue #3)

--- web/js/script.js
+++ web/js/script.js
@@ -1,8 +1,4 @@
 
-if (typeof VK !== 'undefined') {
-    VK.init({apiId: 4540646});
-}
-
 function resizeVkAppWindow() {
     if (typeof VK !== 'undefined') {
         var newheight = $('#main-container').height();
@@ -14,14 +10,16 @@ function numberWithCommas(x) {
   return x.toString().replace(/\B(?=(\d{3})+(?!\d))/g, " ");
 }
 
-$(function () {    
-    if (typeof VK !== 'undefined') {
-        setInterval(resizeVkAppWindow, 500);
-        $.ajaxStop(resizeVkAppWindow);
+$(function () {
+    if (self.parent != self) { // мы в приложении вк    
+        if (typeof VK !== 'undefined') {
+            
+            VK.init({apiId: 4540646});
+            
+            setInterval(resizeVkAppWindow, 500);
+            $.ajaxStop(resizeVkAppWindow);
+        }
     }
-//    if (self.parent.frames.length === 0) { // мы не в приложении вк
-//        $('#account_settings_button').show();
-//    }
     
     $('.commas-input').on('keyup', function(){
         $(this).val(numberWithCommas($(this).val().replace(/\s/g,'')));
